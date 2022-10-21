2f7073b9ab11c3274b965af3ed2f60e6a3c10dcf
Tamirlan <gabaraev@sfedu.ru>
2022-10-21 20:47:36 +0300

    change password and port in db

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import { Pool } from 'pg';
 import { PythonShell } from 'python-shell'
 
 
@@ -26,38 +27,83 @@ app.use(function (req, res, next) {
   next();
 });
 
-const messages = [
+const pool = new Pool({
+  user: 'postgres',
+  password: 'insert',
+  host: 'localhost',
+  port: 1234,
+  database: 'postgres'
+});
+
+const newMessage = [
     {
-      user: 'bot',
-      content: `Привет, чем я могу тебе помочь?`
+      "user": "bot",
+      "content": "Привет, чем я могу тебе помочь?"
     }
   ]
  
 app.get('/', (req, res) => {
+  pool.query(`SELECT messages from chats WHERE id = ${req.query.id}`, (err, queryRes) => {
+    if (err)
+      console.error(err)
+
+    if (queryRes.rows[0] === undefined) {
+      pool.query(`INSERT INTO chats VALUES(${req.query.id}, '${JSON.stringify(newMessage)}')`, (err, queryRes) => {
+        if (err)
+          console.error(err)
+      })
+      res.status(200).send(newMessage)
+      return
+    }
+    const messages = queryRes.rows[0].messages
+    for (let i = 0; i < messages.length; i++)
+      messages[i].content = messages[i].content.replaceAll('`', '"')
+
     res.status(200).send(messages)
+  })
     
 })
 
 app.post('/', (req, res) => {
-  messages.push(req.body)
-  
+  //inserting incoming user message
+
   const options = {
     args: [req.body.content]
 };
-
-  PythonShell.run('Ml_chatbot/ML for chatbot.py', options, (err, output) => {
+  PythonShell.run('./Ml_chatbot/chatbot.py', options, (err, output) => {
     if (err)
       console.error(err);
 
     if (typeof output !== 'undefined') {
-      const doubleQuotesOutput = output[0].replaceAll("'", '"')
+      const doubleQuotesOutput = output[0]
+      .replaceAll("'", '"')
+      .replaceAll('\\xa0', '') // breaks server 
       const answer = JSON.parse(doubleQuotesOutput)
       const messageContent = answer.content.replaceAll('`', '"')
-      messages.push( {
-        user: 'bot',
-        content: messageContent
-      })   
+  
       res.status(200).send({type: answer.type, content: messageContent})
+      // after sending message to the front: UPDATE it on the table
+      pool.query(`SELECT messages FROM chats WHERE id = ${req.query.id}`, (err, queryRes) => {
+        if (err)
+          console.error(err);
+        const messages = queryRes.rows[0].messages
+        messages.push({
+          "user": "user",
+          "content": req.body.content
+        })
+        messages.push({
+          "user": "bot",
+          "content": messageContent
+        })
+
+        pool.query(
+          `UPDATE chats
+          SET messages = '${JSON.stringify(messages)}'
+          WHERE id = ${req.query.id}`, (err, queryRes) => {
+            if (err)
+              console.error(err)
+          })
+      })
     }
   })
 })
